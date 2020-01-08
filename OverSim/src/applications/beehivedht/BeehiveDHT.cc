71c350929a04de5e4eec5628d52c8e553ba48f04
Andrew Jones <ajones788@gmail.com>
2020-01-08 08:57:43 -0600

    committing mistakes

--- OverSim/src/applications/beehivedht/BeehiveDHT.cc
+++ OverSim/src/applications/beehivedht/BeehiveDHT.cc
@@ -859,7 +859,7 @@ void BeehiveDHT::handleReplicateTimerExpired(cMessage* msg)
 void BeehiveDHT::handleReplicateRequest(BeehiveReplicateCall* replicateRequest) 
 {
 
-	std::cout << replicateRequest.getMessageType();
+	//std::cout << replicateRequest.getType();
     // TODO: request contains list of replicated keys
 
     // TODO: compare incoming key list to the keys stored at this node, and store two list:
